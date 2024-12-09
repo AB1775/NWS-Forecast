6d2f7c622bd210f4525b0d4b9488dd175d776032
AB1775 <66264218+AB1775@users.noreply.github.com>
2024-12-09 11:33:14 -0600

    Update Classes.h
    Class Descriptions
    --------------------------
    GridPoints: Represents a single grid point with latitude and longitude
    
    Location: Represents a single location with the zip code, full state name & state abbreviation, latitude, longitude, and city
    
    ForecastPeriod: Represents a single forecast period with a name, current or projected temperature, current or projected wind speeds and directions, a short forecast, and a detailed forecast
    
    Forecast: Represents a completed forecast, containing a vector of the type "ForecastPeriod" made up of 12 (for a 6-day forecast) individual periods

--- include/Classes.h
+++ include/Classes.h
@@ -1,1 +1,52 @@
+#pragma once
 
+#include <iostream>
+
+/**************************************************************************
+ * @brief Class representing a grid point with latitude and longitude coordinates.
+ **************************************************************************/
+
+class GridPoints
+{
+public:
+    double latitude;
+    double longitude;
+};
+
+/**************************************************************************
+ * @brief Class representing a location entity
+ **************************************************************************/
+class Location
+{
+public:
+    std::string zipCode;
+    std::string state_abbreviation;
+    std::string latitude;
+    std::string longitude;
+    std::string city;
+    std::string state;
+};
+
+/**************************************************************************
+ * @brief Class representing a single forecast period with temperature, wind, and forecast details
+ **************************************************************************/
+class ForecastPeriod
+{
+    public:
+        std::string name;
+        int temperature;
+        std::string temperatureUnit;
+        std::string windSpeed;
+        std::string windDirection;
+        std::string shortForecast;
+        std::string detailedForecast;
+};
+
+/**************************************************************************
+ * @brief Class representing a forecast with multiple periods.
+ **************************************************************************/
+class Forecast
+{
+    public:
+        std::vector<ForecastPeriod> periods;
+};
